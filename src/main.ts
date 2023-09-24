1eb8a9273f346d8c9de3a10a3f7a580fb2f79384
Noam Bergauz <bergauznoam@gmail.com>
2023-09-24 11:54:39 +0300

    Updated swagger path to docs instead of api

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,9 @@ async function bootstrap() {
     .setTitle('Me2Gift')
     .setDescription('Me2Gift API Documentation')
     .setVersion('1.0')
-    // .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
