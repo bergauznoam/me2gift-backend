9c81ac002ce03e864e5c99c0ca1140320d587e30
Noam Bergauz <bergauznoam@gmail.com>
2023-09-24 14:51:39 +0300

    Added global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CustomExceptionFilter } from './exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api', { exclude: ['docs'] });
+  app.useGlobalFilters(new CustomExceptionFilter());
   const config = new DocumentBuilder()
     .setTitle('Me2Gift')
     .setDescription('Me2Gift API Documentation')
